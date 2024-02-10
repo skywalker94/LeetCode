9e7abcab0e2a4b3ada401dbc59048d9d169ab459
Sharangdeo Bhagwat <skywalker94@users.noreply.github.com>
2024-02-10 17:28:38 +0000

    Update valid_palindrome.py

--- Solutions/valid_palindrome.py
+++ Solutions/valid_palindrome.py
@@ -6,4 +6,19 @@ class Solution(object):
         p = ''.join(p)
         return True if p == p[::-1] else False
 
-# Two pointer solution
+# # Two pointer solution
+# class Solution(object):
+#     def isPalindrome(self, s):
+#         s = s.strip().lower()
+#         left = 0
+#         right = len(s) - 1
+#         while left < right:
+#             while not s[left].isalnum() and left < right:
+#                 left += 1
+#             while not s[right].isalnum() and left < right:
+#                 right -= 1
+#             if s[left] != s[right]:
+#                 return False
+#             left += 1
+#             right -= 1
+#         return True
