32976260359b1b4f073b124fd17b5fd139904533
Sharangdeo Bhagwat <skywalker94@users.noreply.github.com>
2024-02-10 17:30:01 +0000

    Update palindrome_number.py

--- Solutions/palindrome_number.py
+++ Solutions/palindrome_number.py
@@ -1,7 +1,19 @@
 # https://leetcode.com/problems/palindrome-number/
 
-# Check the palindrome_number_no_string.py file for the no converting to string approach
-
 class Solution(object):
     def isPalindrome(self, x):
         return str(x) == str(x)[::-1]
+
+# # not conversion to string approach
+# class Solution(object):
+#     def isPalindrome(self, x):
+#         exponent = 0
+#         old = x
+#         new = 0
+#         while True:
+#             new = (new*10) + (x % 10)
+#             x = (x - (x% 10)) / 10
+#             exponent += 1
+#             if (old / 10**exponent) < 1:
+#                 break
+#         return new == old
