9beaa4102dd719d08cfaa6a986aac7b01e9612b8
Sharangdeo Bhagwat <skywalker94@users.noreply.github.com>
2024-02-10 14:56:49 +0000

    Update longest_substring_without_repeating_characters.py
    Added a readable solution

--- Solutions/longest_substring_without_repeating_characters.py
+++ Solutions/longest_substring_without_repeating_characters.py
@@ -14,3 +14,30 @@ class Solution(object):
             letters[c] = True
             max_size = max(max_size, len(letters))
         return max_size
+
+# # For readability
+# from collections import OrderedDict
+# class Solution(object):
+#     def remove_letters(self, letters, char): # done
+#         for l in letters:
+#             letters.pop(l)
+#             if l == char:
+#                 break
+#         return letters
+
+#     def read_in(self, letters, char):
+#         if char not in letters:
+#             letters[char] = True
+#             return letters
+#         letters = self.remove_letters(letters, char)
+#         letters[char] = True
+#         return letters
+
+#     def lengthOfLongestSubstring(self, s):
+#         left = 0
+#         max_size = 0
+#         letters = OrderedDict()
+#         for c in s:
+#             letters = self.read_in(letters, c)
+#             max_size = max(max_size, len(letters))
+#         return max_size
